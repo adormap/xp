58ae6727a8f1673f82e4b114968febdb7222223a
adormap <35962696+adormap@users.noreply.github.com>
2022-02-11 14:41:33 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -2,14 +2,6 @@
 
 	include 'db.php';
 	include 'start.php';
-	include 'help.php';
-	include 'mjo.php';
-	include 'satir.php';
-	include 'satcv.php';
-	include 'satvis.php';
-	include 'satvir.php';
-	include 'ecm.php';
-
 
 	file_get_contents($url);
 
